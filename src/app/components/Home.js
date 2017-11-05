de5db589ef84f93e83979f171db078b4616af69d
Walter Hietanen <hietanen@gmail.com>
2017-11-05 22:45:01 +0100

    this 2

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -2,19 +2,9 @@ import React from "react";
 
 export class Home extends React.Component {
   render() {
-
     return (
       <div>
         <p>Your name is {this.props.name}, your age is {this.props.age}</p>
-        <p>User object => Name: {this.props.user.name}</p>
-        <div>
-        <h4>Hobbies</h4>
-          <ul>
-            {this.props.user.hobbies.map((hobby, i) => <li key={i}>{hobby}</li>)}
-          </ul>
-        </div>
-
-        {this.props.children}
       </div>
     );
   }
@@ -22,7 +12,5 @@ export class Home extends React.Component {
 
 Home.propTypes = {
   name: React.PropTypes.string,
-  age: React.PropTypes.number,
-  user: React.PropTypes.object,
-  children: React.PropTypes.element.isRequired
+  age: React.PropTypes.number
 };
